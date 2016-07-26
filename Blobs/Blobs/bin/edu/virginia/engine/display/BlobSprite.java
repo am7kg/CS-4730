0e6ea7580f09569ee861f687a912d1ce8f288407
aliswagmaster <ac8pn@virginia.edu>
2016-07-26 15:29:48 -0400

    Add files via upload

---
+++ Blobs/Blobs/bin/edu/virginia/engine/display/BlobSprite.java
@@ -0,0 +1,10 @@
+package edu.virginia.engine.display;
+
+public class BlobSprite extends AnimatedSprite {
+
+	public BlobSprite(String id, String fileName) {
+		super(id, fileName);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Blobs/Blobs/bin/edu/virginia/engine/display/ItemSprite.java
@@ -0,0 +1,10 @@
+package edu.virginia.engine.display;
+
+public class ItemSprite extends AnimatedSprite {
+
+	public ItemSprite(String id, String fileName) {
+		super(id, fileName);
+		// TODO Auto-generated constructor stub
+	}
+
+}
